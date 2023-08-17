ac4f77a9b2d7f00bf20f9935150681a024b800b2
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-08-17 15:44:05 -0300

    REFACTOR: Added name to the user.id constraint

--- user/models/User.py
+++ user/models/User.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from sqlalchemy import UniqueConstraint, CheckConstraint
 
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -11,8 +12,10 @@ table = sqlalchemy.Table(
     sqlalchemy.Column("id", UUID(as_uuid=True),
                       primary_key=True,
                       # The code below will call for a native function when used by the ORM to interact with PostgreSQL
-                      server_default=sqlalchemy.text("gen_random_uuid()"),
-                      unique=True),
+                      server_default=sqlalchemy.text("gen_random_uuid()")),
     sqlalchemy.Column("first_name", sqlalchemy.String),
     sqlalchemy.Column("last_name", sqlalchemy.String),
+
+    # This strategy allows to set a name to the constraint
+    UniqueConstraint("id", name="id_unique"),
 )
