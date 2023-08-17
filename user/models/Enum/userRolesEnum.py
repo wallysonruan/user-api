e06ac25f703df99287335f310269941c0e833e70
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-08-17 18:43:22 -0300

    FEAT: is_in and to_string to enum

--- user/models/Enum/userRolesEnum.py
+++ user/models/Enum/userRolesEnum.py
@@ -3,4 +3,12 @@ import enum
 
 class UserRolesEnum(enum.Enum):
     user: str = "usr"
-    admin: str = "adm"
+    admin: str = 'adm'
+
+    @classmethod
+    def is_string_in_enum(cls, text: str) -> bool:
+        return text in [role.value for role in UserRolesEnum]
+
+    @classmethod
+    def to_string(cls) -> dict:
+        return [role.value for role in UserRolesEnum]
